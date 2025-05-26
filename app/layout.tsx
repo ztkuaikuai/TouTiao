6a18e7e26a3ae4e97cbe25321cc3669209b5d0ed
ztkuaikuai <2647731903@qq.com>
2025-05-26 20:10:45 +0800

    fix: 修复主页水合错误

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "今日头条 - 你关心的，才是头条",
   description: "今日头条是一个通用信息平台，致力于连接人与信息，让优质丰富的信息得到高效精准的分发",
-    generator: 'v0.dev'
+  generator: 'ztkuaikuai/v0.dev'
 }
 
 export default function RootLayout({
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="zh-CN">
+    <html lang="zh-CN" className="light" style={{colorScheme:"light"}}>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
